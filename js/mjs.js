fdc7e06e2c5955708841faa4d2b95a26d9986454
wagentim <bhwerbung@gmail.com>
2014-10-07 22:53:39 +0200

    add js file

---
+++ js/mjs.js
@@ -0,0 +1,11 @@
+/**
+ * @author wagentim
+ */
+
+$(document).ready(function() {
+	// TODO add some initial codes
+});
+function getTime() {
+	var d = new Date();
+	$(".fixed_headerbar_content").html(d);
+}

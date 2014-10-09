8818d496cc91e94d1827891d04cd527d246c5dd1
wagentim <bhwerbung@gmail.com>
2014-10-09 22:49:35 +0200

    create a brief outline for the user.php page

--- index.php
+++ index.php
@@ -13,19 +13,78 @@
 		<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
 		<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.11.1/jquery-ui.min.js"></script>
 		<script src="js/mjs.js"></script>
+<script>
+	$(function() {
+		$("#speed").selectmenu();
+		$("#files").selectmenu();
+		$("#number").selectmenu().selectmenu("menuWidget").addClass("overflow");
+	});
+		</script>
 		<title>Home Apps</title>
 	</head>
-
 	<body>
 		<div class="header_wrap">
 			<div class="header_content">
 				<nav>
-					<ul>
-						<li>Home Apps</li>
-						<li>Business System</li>
-					</ul>
+					<a href="main.php">Home Apps</a> | 
+					<a>代购系统</a>
 				</nav>
 			</div>
 		</div>
+		<div class="left_block">
+			显示大概的统计数据
+		</div>
+		<!-- main area -->
+		
+		<!-- the operation area -->
+		<
+		<div class="demo">
+			<form action="#">
+				<fieldset>
+					<label for="speed">Select a speed</label>
+					<select name="speed" id="speed">
+						<option>Slower</option>
+						<option>Slow</option>
+						<option selected="selected">Medium</option>
+						<option>Fast</option>
+						<option>Faster</option>
+					</select>
+					<label for="files">Select a file</label>
+					<select name="files" id="files">
+						<optgroup label="Scripts">
+							<option value="jquery">jQuery.js</option>
+							<option value="jqueryui">ui.jQuery.js</option>
+						</optgroup>
+						<optgroup label="Other files">
+							<option value="somefile">Some unknown file</option>
+							<option value="someotherfile">Some other file with a very long option text</option>
+						</optgroup>
+					</select>
+					<label for="number">Select a number</label>
+					<select name="number" id="number">
+						<option>1</option>
+						<option selected="selected">2</option>
+						<option>3</option>
+						<option>4</option>
+						<option>5</option>
+						<option>6</option>
+						<option>7</option>
+						<option>8</option>
+						<option>9</option>
+						<option>10</option>
+						<option>11</option>
+						<option>12</option>
+						<option>13</option>
+						<option>14</option>
+						<option>15</option>
+						<option>16</option>
+						<option>17</option>
+						<option>18</option>
+						<option>19</option>
+					</select>
+				</fieldset>
+			</form>
+		</div>
+		<div class="table"></div>
 	</body>
 </html>
